301f06b530bace1486fd2e596e6b765d3d290f61
wnchen <cwb105977@gmail.com>
2016-07-12 18:38:29 -0400

    ADD an application for some third-party exception detection

---
+++ app/src/main/java/com/wenbchen/android/imdb/application/IMDBSearchApplication.java
@@ -0,0 +1,14 @@
+package com.wenbchen.android.imdb.application;
+
+import android.app.Application;
+
+/**
+ * Created by wenbchen on 7/12/16.
+ */
+public class IMDBSearchApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
